b37625702e224485df1eefde8db261d06c784bd3
gara-age <minseo00515@naver.com>
2021-10-10 11:34:00 +0900

    피자가게 어댑터 뼈대 작업 완료

--- app/src/main/java/com/example/pizzaorderapp_20211010/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/adapters/PizzaStoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.pizzaorderapp_20211010.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.pizzaorderapp_20211010.R
 import com.example.pizzaorderapp_20211010.datas.StoreData
 
 class PizzaStoreAdapter(
@@ -11,4 +15,20 @@ class PizzaStoreAdapter(
 
 
 ) : ArrayAdapter<StoreData>(mContext,resId,mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = mInflater.inflate(R.layout.pizza_store_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
 }
