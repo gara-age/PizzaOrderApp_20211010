9c9e34703e63d497dd76554aee88fc76c94bef5c
gara-age <minseo00515@naver.com>
2021-10-10 14:19:51 +0900

    startActivityForResult 로 닉네임 가지러 이동

--- app/src/main/java/com/example/pizzaorderapp_20211010/fragment/MyProfileFragment.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/fragment/MyProfileFragment.kt
@@ -1,14 +1,18 @@
 package com.example.pizzaorderapp_20211010.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.pizzaorderapp_20211010.EditNicknameActivity
 import com.example.pizzaorderapp_20211010.R
 import kotlinx.android.synthetic.main.fragment_my_prohile.*
 
 class MyProfileFragment : Fragment() {
+
+    val REQ_FOR_NICKNAME = 1010
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,8 +27,9 @@ class MyProfileFragment : Fragment() {
 
         editNicknameBtn.setOnClickListener {
 
+        val myIntent = Intent(requireContext(),EditNicknameActivity::class.java)
 
-
+            startActivityForResult(myIntent,REQ_FOR_NICKNAME)
         }
 
 
