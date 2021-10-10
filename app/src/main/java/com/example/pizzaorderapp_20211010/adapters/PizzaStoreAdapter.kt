2da7c76a8124e2cd71a9b25e38690d4c60b5095d
gara-age <minseo00515@naver.com>
2021-10-10 11:53:14 +0900

    Glide로 로고 불러내기   - 파파존스 이미지는 안불러짐

--- app/src/main/java/com/example/pizzaorderapp_20211010/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/adapters/PizzaStoreAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.pizzaorderapp_20211010.R
 import com.example.pizzaorderapp_20211010.datas.StoreData
 
@@ -37,6 +38,7 @@ class PizzaStoreAdapter(
 
         storeNameTxt.text = data.name
 
+        Glide.with(mContext).load(data.logoURL).into(logoImg)
 
         return row
 
