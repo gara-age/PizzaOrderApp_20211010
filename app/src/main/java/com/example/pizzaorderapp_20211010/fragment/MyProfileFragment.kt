0c3e53d7df8109625379d88d61f6c7efb20f7920
gara-age <minseo00515@naver.com>
2021-10-10 14:39:52 +0900

    프래그먼트로 돌아와서 -> 새 닉네임 반영

--- app/src/main/java/com/example/pizzaorderapp_20211010/fragment/MyProfileFragment.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/fragment/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.pizzaorderapp_20211010.fragment
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -34,6 +35,21 @@ class MyProfileFragment : Fragment() {
 
 
 
+
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_FOR_NICKNAME){
+
+            if(resultCode == Activity.RESULT_OK){
+                val newNick = data!!.getStringExtra("nickname")
+                nicknameTxt.text = newNick
+
+            }
+
+        }
+
+    }
 }
